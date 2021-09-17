69a657d81e0b285e8195e8a7780594d3edb6608b
Joao Dias <joaoeduardodias123@gmail.com>
2021-09-17 09:25:15 -0300

    feat: page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 
 import Head from 'next/head'
-
+import styes from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-    
+      <main className={styes.contentContainer} >
+        <section className={styes.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span>word.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+
     </>
   )
 }
