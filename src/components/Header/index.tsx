02c8677eb25a63d211f2600cfe938ca53a88be77
Joao Dias <joaoeduardodias123@gmail.com>
2021-12-08 11:49:57 -0300

    feat: page post

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,6 @@ import styles from './styles.module.scss'
 import { ActiveLink } from '../ActiveLink'
 export function Header(){
 
-
-
-
 return (
   <header className={styles.headerContainer}>
     <div className={styles.headerContent}>
