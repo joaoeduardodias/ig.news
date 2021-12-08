81b201832d0ad9f247a98cda24279608d9765dc0
Joao Dias <joaoeduardodias123@gmail.com>
2021-12-08 13:41:35 -0300

    feat: verify subscription active

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
@@ -10,7 +11,7 @@ interface SubscribeButtonProps {
 export function SubscribeButton({priceId}:SubscribeButtonProps) {
 
   const [session] = useSession();
-
+  const router = useRouter()
 
   async function handleSubscribe() {
 
@@ -18,6 +19,14 @@ export function SubscribeButton({priceId}:SubscribeButtonProps) {
       signIn('github')
       return;
     }
+
+    if(session.activeSubscription) {
+      
+      router.push("/posts")
+      return;
+    }
+
+
     try {
       const response = await api.post('/subscribe')
 
